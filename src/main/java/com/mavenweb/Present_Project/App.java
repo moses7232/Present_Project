edbfb5d949b999b7db74c9101359ce9024298d4b
moses7232 <145208747+moses7232@users.noreply.github.com>
2024-01-16 14:08:10 +0530

    Update2 App.java

--- src/main/java/com/mavenweb/Present_Project/App.java
+++ src/main/java/com/mavenweb/Present_Project/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello i made changes" );
+        System.out.println( "Hello i made changes second time" );
     }
 }
